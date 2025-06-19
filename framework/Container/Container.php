7486725605bf4f1bc26793e3584044493a23480f
STones_ <61647649@qq.com>
2025-06-19 15:00:28 +0800

    init

--- framework/Container/Container.php
+++ framework/Container/Container.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Framework\Container;
+namespace Aries\Container;
 
 use Closure;
 use ReflectionClass;
@@ -17,7 +17,7 @@ class Container
     protected array $bindings = [];
 
     /**
-     * 共享实例（单例）
+     * 已解析的实例
      *
      * @var array
      */
@@ -28,7 +28,7 @@ class Container
      *
      * @var self
      */
-    protected static ?self $instance = null;
+    private static ?self $instance = null;
 
     /**
      * 获取容器单例
@@ -45,189 +45,158 @@ class Container
     }
 
     /**
-     * 设置容器单例
-     *
-     * @param Container|null $container
-     * @return Container
+     * 重置容器单例
+     * 
+     * @return void
      */
-    public static function setInstance(?Container $container = null): Container
+    public static function reset(): void
     {
-        return static::$instance = $container;
+        static::$instance = null;
     }
 
     /**
-     * 绑定接口到实现
-     *
-     * @param string $abstract
-     * @param Closure|string|null $concrete
-     * @param bool $shared
+     * 绑定到容器
+     * 
+     * @param string $abstract 绑定的标识符
+     * @param mixed $concrete 具体的实现
      * @return void
+     * 
+     * @example
+     * // 1. 绑定接口到实现
+     * $container->bind(LoggerInterface::class, FileLogger::class);
+     * 
+     * // 2. 绑定闭包（支持参数）
+     * $container->bind('api', function($container, ...$parameters) {
+     *     return new ApiClient(...$parameters);
+     * });
+     * 
+     * // 3. 绑定实例
+     * $container->bind('config', new Config());
+     * 
+     * // 4. 绑定单例
+     * $container->bind(FileLogger::class); 等同于 $container->bind(FileLogger::class, FileLogger::class);
      */
-    public function bind(string $abstract, $concrete = null, bool $shared = false): void
+    public function bind(string $abstract, $concrete = null): void
     {
-        // 删除过期的实例
-        $this->dropStaleInstances($abstract);
+        // 清除已解析的实例缓存
+        unset($this->instances[$abstract]);
 
-        // 如果未提供具体实现，则使用抽象作为具体实现
+        // 如果没有提供实现，则使用抽象本身
         if (is_null($concrete)) {
             $concrete = $abstract;
         }
 
-        // 如果具体实现不是闭包，则获取闭包
-        if (!$concrete instanceof Closure) {
-            $concrete = $this->getClosure($abstract, $concrete);
+        // 如果是类名字符串，转换为闭包
+        if (is_string($concrete)) {
+            $concrete = function ($container, ...$parameters) use ($concrete) {
+                return $container->build($concrete, ...$parameters);
+            };
         }
 
-        $this->bindings[$abstract] = compact('concrete', 'shared');
+        // 会覆盖之前的绑定关系
+        $this->bindings[$abstract] = $concrete;
     }
 
-    /**
-     * 绑定单例
-     *
-     * @param string $abstract
-     * @param Closure|string|null $concrete
-     * @return void
-     */
-    public function singleton(string $abstract, $concrete = null): void
-    {
-        $this->bind($abstract, $concrete, true);
-    }
-
-    /**
-     * 绑定已存在的实例
-     *
-     * @param string $abstract
-     * @param mixed $instance
-     * @return mixed
-     */
-    public function instance(string $abstract, mixed $instance): mixed
-    {
-        $this->dropStaleInstances($abstract);
-
-        $this->instances[$abstract] = $instance;
-
-        return $instance;
-    }
 
     /**
      * 解析依赖
      *
      * @param string $abstract
-     * @param array $parameters
+     * @param mixed ...$parameters
      * @return mixed
      * @throws Exception
      */
-    public function make(string $abstract, array $parameters = []): mixed
+    public function make(string $abstract, ...$parameters): mixed
     {
-        return $this->resolve($abstract, $parameters);
+        return $this->resolve($abstract, ...$parameters);
     }
 
-    /**
-     * 判断是否已绑定
-     *
-     * @param string $abstract
-     * @return bool
-     */
-    public function bound(string $abstract): bool
-    {
-        return isset($this->bindings[$abstract]) ||
-               isset($this->instances[$abstract]);
-    }
 
     /**
-     * 获取闭包
+     * 解析依赖, 返回实例
      *
      * @param string $abstract
-     * @param string $concrete
-     * @return Closure
-     */
-    protected function getClosure(string $abstract, string $concrete): Closure
-    {
-        return function ($container, $parameters = []) use ($abstract, $concrete) {
-            if ($abstract === $concrete) {
-                return $container->build($concrete);
-            }
-
-            return $container->resolve($concrete, $parameters);
-        };
-    }
-
-    /**
-     * 解析依赖
-     *
-     * @param string $abstract
-     * @param array $parameters
+     * @param mixed ...$parameters
      * @return mixed
      * @throws Exception
      */
-    protected function resolve(string $abstract, array $parameters = []): mixed
+    protected function resolve(string $abstract, ...$parameters): mixed
     {
-        // 如果是已注册的实例，直接返回
+        // 如果已经有缓存的实例，直接返回
         if (isset($this->instances[$abstract])) {
             return $this->instances[$abstract];
         }
 
-        $concrete = $this->getConcrete($abstract);
-
-        // 如果是共享实例（单例），则缓存实例
-        if ($this->isShared($abstract)) {
-            $object = $this->build($concrete, $parameters);
-            return $this->instances[$abstract] = $object;
+        // 如果没有绑定关系，抛出异常
+        if (!isset($this->bindings[$abstract])) {
+            throw new Exception("Target [$abstract] is not bound in the container.");
         }
 
-        return $this->build($concrete, $parameters);
+        $concrete = $this->bindings[$abstract];
+
+        // 如果绑定的是闭包，执行它
+        if ($concrete instanceof Closure) {
+            $object = $concrete($this, ...$parameters);
+        } 
+        // 如果绑定的是对象实例，直接使用
+        else if (is_object($concrete)) {
+            $object = $concrete;
+        }
+        // 其他情况（这种情况不应该发生，因为字符串在bind时已经转换为闭包）
+        else {
+            throw new Exception("Invalid binding for [$abstract]");
+        }
+
+        // 缓存实例
+        $this->instances[$abstract] = $object;
+        return $object;
     }
 
     /**
-     * 获取实际的类名或闭包
+     * 构建实例, 只有在bind的时候传入的是类名时才会调用
      *
-     * @param string $abstract
-     * @return mixed
-     */
-    protected function getConcrete(string $abstract): mixed
-    {
-        if (isset($this->bindings[$abstract])) {
-            return $this->bindings[$abstract]['concrete'];
-        }
-
-        return $abstract;
-    }
-
-    /**
-     * 构建实例
-     *
-     * @param Closure|string $concrete
-     * @param array $parameters
-     * @return mixed
+     * @param string $concrete 要构建的类名
+     * @param mixed ...$parameters 构造函数参数
+     * @return object
      * @throws Exception
      */
-    protected function build($concrete, array $parameters = []): mixed
+    protected function build(string $concrete, ...$parameters): object
     {
-        if ($concrete instanceof Closure) {
-            return $concrete($this, $parameters);
-        }
-
         try {
             $reflector = new ReflectionClass($concrete);
         } catch (Exception $e) {
             throw new Exception("Target class [$concrete] does not exist.", 0, $e);
         }
 
+        // 是否可以被实例化
         if (!$reflector->isInstantiable()) {
             throw new Exception("Target [$concrete] is not instantiable.");
         }
 
-        $constructor = $reflector->getConstructor();
+        // 1. 如果提供了参数，直接使用这些参数创建实例
+        if (!empty($parameters)) {
+            return $reflector->newInstanceArgs($parameters);
+        }
 
-        if (is_null($constructor)) {
+
+        // 2. 如果没有传递构造函数的参数，那我们就需要通过获取构造函数来解析依赖
+
+        // 2.1. 获取构造函数
+        $constructor = $reflector->getConstructor();
+        if (is_null($constructor)) { // 没有构造函数，直接返回实例
             return new $concrete;
         }
 
+        // 2.2. 获取构造函数的参数
         $dependencies = $constructor->getParameters();
+
+        // 2.3. 解析依赖
         $instances = $this->resolveDependencies($dependencies);
 
+        // 2.4. 返回实例
         return $reflector->newInstanceArgs($instances);
     }
+
 
     /**
      * 解析构造函数的依赖
@@ -250,6 +219,9 @@ class Container
                     throw new Exception("Unresolvable dependency resolving [$dependency] in class {$dependency->getDeclaringClass()->getName()}");
                 }
             } else {
+                if (!$this->bound($type->getName())) {
+                    throw new Exception("Dependency [{$type->getName()}] is not bound in the container.");
+                }
                 $results[] = $this->make($type->getName());
             }
         }
@@ -258,26 +230,14 @@ class Container
     }
 
     /**
-     * 判断是否是共享实例
+     * 判断是否已绑定
      *
      * @param string $abstract
      * @return bool
      */
-    protected function isShared(string $abstract): bool
+    protected function bound(string $abstract): bool
     {
-        return isset($this->instances[$abstract]) ||
-            (isset($this->bindings[$abstract]['shared']) &&
-                $this->bindings[$abstract]['shared'] === true);
-    }
-
-    /**
-     * 删除过期的实例
-     *
-     * @param string $abstract
-     * @return void
-     */
-    protected function dropStaleInstances(string $abstract): void
-    {
-        unset($this->instances[$abstract]);
+        return isset($this->bindings[$abstract]) ||
+               isset($this->instances[$abstract]);
     }
 } 
